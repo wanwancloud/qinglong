b6a4e1b8cb10a78467f5562d59404f54a69df822
Zi <139439938+wanwancloud@users.noreply.github.com>
2024-04-30 14:53:27 +0800

    Update fenrunbao.py

--- fenrunbao.py
+++ fenrunbao.py
@@ -1,20 +1,20 @@
 import requests, os, time, random, re
 
+def version():
+    txt = requests.get("https://gitee.com/jd_688/zi/raw/master/ziiii.txt").text
+    print(txt)
 
 class yuanshen():
     def __init__(self) -> None:
         self.headers = {
             "Host": "lfb.ihuju.cn",
             "Connection": "keep-alive",
-            "Content-Length": "188",
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "X-Requested-With": "XMLHttpRequest",
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Origin": "http://lfb.ihuju.cn",
-            "Referer": "http://lfb.ihuju.cn/index.php/Home/Fenrun/index.html",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Upgrade-Insecure-Requests": "1",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Mobile/15E148 Safari/604.1",
+            "Referer": "http://lfb.ihuju.cn/index.php/Home/My/index.html",
+            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+            "Accept-Encoding": "gzip, deflate"
         }
         self.cookies = {
             "BJYADMIN": "lhdgqh13qlms2d1to95kge52j6",
@@ -31,12 +31,12 @@ class yuanshen():
             "Pragma": "no-cache",
             "Referer": "http://lfb.ihuju.cn/",
             "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Mobile/15E148 Safari/604.1"
         }
         url = "http://lfb.ihuju.cn/index.php/Home/Fenrun/index.html"
-        r = requests.get(url, headers=headers, cookies=self.cookies)  # 获取页面内容
+        r = requests.get(url, headers=headers, cookies=self.cookies)
         if r.status_code == 200:
-            rule = r'\$\.\w+\(url,{([^:]+)'  # 匹配以字母或下划线开头的变量名，冒号，以及冒号后的内容（直到逗号或右括号为止）
+            rule = r'\$\.\w+\(url,{([^:]+)'
             match = re.search(rule, r.text)
             if match:
                 key = match.group(1)
@@ -47,23 +47,23 @@ class yuanshen():
         else:
             print("请求失败，状态码：", r.status_code)
 
-    def watch_video(self):  # 看广告
+    def watch_video(self):
         i = 1
         while True:
-            url = "http://lfb.ihuju.cn/index.php/Home/Fenrun/sign.html"  # 广告页面url
+            url = "http://lfb.ihuju.cn/index.php/Home/Fenrun/sign.html"
             data = f"{self.key}={i}"
-            r = requests.post(url, headers=self.headers, data=data, cookies=self.cookies).json()  # 提交广告请求
+            r = requests.post(url, headers=self.headers, data=data, cookies=self.cookies).json()
             if r['status'] == 1:
-                print(f"领取第[{i}]级奖励成功")
+                print(f"🎉 领取第[{i}]级奖励成功")
             else:
-                print(f"领取第[{i}]级奖励失败----[{r['info']}]")
+                print(f"🎉 领取第[{i}]级奖励失败----[{r['info']}]")
                 if r['info'] != "请在刷新页面重试":
                     break
             time.sleep(random.randint(65, 80))
             i += 1
-            if i == 39:  # 最多38次 第39次就没了
+            if i == 39:
                 break
 
-
+version()
 main = yuanshen()
 main.watch_video()
